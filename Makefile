914ec61d4d7ad6d95e2e9d657e732305ab4975fb
NishiOwO <nishi@nishi.boats>
2025-11-09 11:36:41 +0000

    version
    git-svn-id: http://svn2.nishi.boats/svn/kleidi/trunk@6 8a844119-c045-4ce2-8fd0-e547c97176bb

--- Makefile
+++ Makefile
@@ -4,13 +4,16 @@ CFLAGS = -I./milsko/include
 LDFLAGS = -L./milsko/lib -Wl,-R./milsko/lib
 LIBS = -lMw
 
-.PHONY: all clean
+.PHONY: all format clean
 .SUFFIXES: .c .o
 
-OBJS = src/main.o src/gui.o src/logo.o
+OBJS = src/main.o src/gui.o src/gui_version.o src/logo.o
 OBJS += external/stb_image.o
 
 all: ./kleidi$(EXEC)
+
+format:
+	clang-format --verbose -i `find src '(' -name "*.c" -or -name "*.h" ')' -and -not -name "logo.c"`
 
 ./kleidi$(EXEC): $(OBJS)
 	$(CC) $(LDFLAGS) -o $@ $(OBJS) $(LIBS)
--- src/gui.c
+++ src/gui.c
@@ -1,74 +1,66 @@
 /* $Id$ */
 #include "kleidi.h"
 
-MwWidget root, window, menu;
-MwMenu menu_file_quit;
-MwMenu menu_help_version;
+#include "../external/stb_image.h"
 
-static void gui_window_close(MwWidget handle, void* user, void* client){
+MwWidget   root, window, menu;
+MwMenu	   menu_file_quit;
+MwMenu	   menu_help_version;
+MwLLPixmap logo_pixmap;
+
+static void gui_window_close(MwWidget handle, void* user, void* client) {
 	gui_confirm_quit();
 }
 
-static void gui_menu_menu(MwWidget handle, void* user, void* client){
-	if(client == menu_file_quit){
+static void gui_menu_menu(MwWidget handle, void* user, void* client) {
+	if(client == menu_file_quit) {
 		gui_confirm_quit();
-	}else if(client == menu_help_version){
+	} else if(client == menu_help_version) {
 		gui_version();
 	}
 }
 
-static void gui_confirm_quit_yes(MwWidget handle, void* user, void* client){
+static void gui_confirm_quit_yes(MwWidget handle, void* user, void* client) {
 	MwDestroyWidget(user);
 	MwDestroyWidget(root);
 }
 
-static void gui_confirm_quit_no(MwWidget handle, void* user, void* client){
+static void gui_confirm_quit_no(MwWidget handle, void* user, void* client) {
 	MwDestroyWidget(user);
 }
 
-void gui_confirm_quit(void){
+void gui_confirm_quit(void) {
 	MwWidget msg = MwMessageBox(window, "Are you sure you want to quit?", "Confirm", MwMB_ICONWARNING | MwMB_BUTTONYESNO);
 
 	MwAddUserHandler(MwMessageBoxGetChild(msg, MwMB_BUTTONYES), MwNactivateHandler, gui_confirm_quit_yes, msg);
 	MwAddUserHandler(MwMessageBoxGetChild(msg, MwMB_BUTTONNO), MwNactivateHandler, gui_confirm_quit_no, msg);
 }
 
-static void gui_version_close(MwWidget handle, void* user, void* client){
-	MwDestroyWidget(handle);
-}
+void gui_init(void) {
+	MwMenu	       m;
+	int	       w, h, ch;
+	unsigned char* data;
 
-void gui_version(void){
-	int w = 600, h = 200;
-	int x = MwGetInteger(window, MwNx) + (MwGetInteger(window, MwNwidth) - w) / 2;
-	int y = MwGetInteger(window, MwNy) + (MwGetInteger(window, MwNheight) - h) / 2;
-	MwWidget version = MwVaCreateWidget(MwWindowClass, "version", root, x, y, w, h,
-		MwNtitle, "Version information",
-	NULL);
-	MwWidget image = MwVaCreateWidget(MwImageClass, "image", version, 10, 10, w - 20, h - 20,
-	NULL);
-
-	MwAddUserHandler(version, MwNcloseHandler, gui_version_close, NULL);
-}
-
-void gui_init(void){
-	MwMenu m;
-
-	root = MwCreateWidget(NULL, "root", NULL, 0, 0, 0, 0);
+	root   = MwCreateWidget(NULL, "root", NULL, 0, 0, 0, 0);
 	window = MwVaCreateWidget(MwWindowClass, "main", root, MwDEFAULT, MwDEFAULT, 1024, 768,
-		MwNtitle, "Kleidi GUI Builder",
-	NULL);
-	menu = MwCreateWidget(MwMenuClass, "menu", window, 0, 0, 0, 0);
+				  MwNtitle, "Kleidi GUI Builder",
+				  NULL);
+	menu   = MwCreateWidget(MwMenuClass, "menu", window, 0, 0, 0, 0);
 
-	m = MwMenuAdd(menu, NULL, "File");
+	m	       = MwMenuAdd(menu, NULL, "File");
 	menu_file_quit = MwMenuAdd(menu, m, "Quit");
 
-	m = MwMenuAdd(menu, NULL, "?Help");
+	m		  = MwMenuAdd(menu, NULL, "?Help");
 	menu_help_version = MwMenuAdd(menu, m, "Version");
+
+	data	    = stbi_load_from_memory(logo, logo_len, &w, &h, &ch, 4);
+	logo_pixmap = MwLoadRaw(window, data, w, h);
+	free(data);
 
 	MwAddUserHandler(window, MwNcloseHandler, gui_window_close, NULL);
 	MwAddUserHandler(menu, MwNmenuHandler, gui_menu_menu, NULL);
 }
 
-void gui_loop(void){
+void gui_loop(void) {
 	MwLoop(root);
 }
