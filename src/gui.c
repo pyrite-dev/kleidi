14e17192f5b90c3fc00f4c8bc4a7ab4ed9ed09c4
NishiOwO <nishi@nishi.boats>
2025-11-11 08:53:31 +0000

    check for size
    git-svn-id: http://svn2.nishi.boats/svn/kleidi/trunk@19 8a844119-c045-4ce2-8fd0-e547c97176bb

--- src/gui.c
+++ src/gui.c
@@ -5,12 +5,12 @@
 
 #include <Mw/Widget/OpenGL.h>
 
-MwWidget   root, window, menu, widgets, logging, opengl, status;
-MwLLPixmap logo_pixmap;
-char	   widget_name[64];
-int	   gui_mode, gui_width, gui_height;
-static MwMenu	   menu_file_quit;
-static MwMenu	   menu_help_version;
+MwWidget      root, window, menu, widgets, logging, opengl, status;
+MwLLPixmap    logo_pixmap;
+char	      widget_name[64];
+int	      gui_mode, gui_width, gui_height;
+static MwMenu menu_file_quit;
+static MwMenu menu_help_version;
 
 static void gui_root_tick(MwWidget handle, void* user, void* client) {
 	gui_opengl_loop();
@@ -153,7 +153,7 @@ void gui_init(void) {
 		h = 768;
 	}
 
-	gui_width = w;
+	gui_width  = w;
 	gui_height = h;
 
 	root	= MwCreateWidget(NULL, "root", NULL, 0, 0, 0, 0);
